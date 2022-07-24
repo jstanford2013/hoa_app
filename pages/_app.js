01cfb9189ed9739e321073a221d45330f02687cf
Jordan <jstanford2013@gmail.com>
2022-07-24 16:10:03 -0500

    Percentage

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,36 @@
 import '../styles/globals.css'
 import Image from 'next/image'
+import React, { useState, useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const [screenSize, getDimension] = useState({
+    dynamicWidth: window.innerWidth,
+    dynamicHeight: window.innerHeight
+  });
+ 
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: window.innerWidth,
+      dynamicHeight: window.innerHeight
+    })
+  }
+   
+  useEffect(() => {
+    window.addEventListener('resize', setDimension);
+     
+    return(() => {
+        window.removeEventListener('resize', setDimension);
+    })
+  }, [screenSize])
+
   return(
     <div style={{display: "flex", justifyItems: "center", alignItems: "center", flexDirection: "column"}}>
       <Image
       alt="Mountains"
       src="/hoa_logo.png"
-      layout="responsive"
+      layout="intrinsic"
+      width={(screenSize.dynamicWidth * .25)}
+      height={(screenSize.dynamicHeight * .25)}
     />
       <Component {...pageProps} />
     </div>
