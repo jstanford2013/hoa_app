97fd22532007b1712a0314ab1382a92f7e55c21f
Jordan <jstanford2013@gmail.com>
2022-07-24 16:31:16 -0500

    put inline background color

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function MyApp({ Component, pageProps }) {
   return(
-    <div style={{display: "flex", justifyItems: "center", alignItems: "center", flexDirection: "column"}}>
+    <div style={{display: "flex", justifyItems: "center", alignItems: "center", flexDirection: "column", backgroundColor:"#faebd7"}}>
 
        <img src={'/hoa_logo.png'} alt="this is car image" style={{justifyItems: "center"}} />
       <Component {...pageProps} />
