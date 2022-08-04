047b0f4e068817ee82e62612ea65abd67498fc43
Jordan <74340273+jstanford2013@users.noreply.github.com>
2022-08-04 09:00:34 -0500

    changed width of logo
    this should fix spacing issue

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function MyApp({ Component, pageProps }) {
   return(
     <div style={{display: "flex", justifyItems: "center", alignItems: "center", flexDirection: "column", backgroundColor:"#fefefe"}}>
-       <img src={'/hoa_logo.png'} alt="this is car image" style={{justifyItems: "center"}} />
+       <img src={'/hoa_logo.png'} alt="this is car image" style={{justifyItems: "center", width: "50%"}} />
       <Component {...pageProps} />
     </div>
   ) 
