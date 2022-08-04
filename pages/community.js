d4ca34622b5679f467a44fcad06468b2545b6b27
Jordan <74340273+jstanford2013@users.noreply.github.com>
2022-08-04 09:20:40 -0500

    Vendor Card breakout
    Broke out the vendor card from the community page and created a components folder location. Also created a css element for the vendor card instead of inline margining

--- pages/community.js
+++ pages/community.js
@@ -1,46 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { Stack, Button } from '@mui/material';
+import { VendorCard } from '../public/components/vendorCard';
 import { useRouter } from 'next/router'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Link from 'next/link';
-
-export function VendorCard(props){
-  const router = useRouter()
-  const handleClick = (value) => {
-    router.push(`${value}`);
-  };
-
-  return(
-    <div>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image={props.image}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {props.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link href={props.site}>
-          <Button size="small">Learn More</Button>
-          </Link>
-        </CardActions>
-      </Card>
-    </div>
-  )
-}
 
 export default function Home() {
   const router = useRouter()
@@ -51,9 +11,9 @@ export default function Home() {
   return (
     <div>
      <Stack style={{alignItems: "center"}}>
-     <VendorCard image="/drone.webp"  title="Drone Business" description="Great Drone Business Cheap!" site="https://exodrones.com"></VendorCard>
-     <VendorCard image="/drone.webp"  title="new Biz" description="Great stuff for you !" site="https://exodrones.com"></VendorCard>
-    <Button variant="outlined"  onClick={() => handleClick("/")} style={{marginTop: "3%"}}>
+      <VendorCard image="/drone.webp"  title="Drone Business" description="Great Drone Business Cheap!" site="https://exodrones.com"></VendorCard>
+      <VendorCard image="/drone.webp"  title="new Biz" description="Great stuff for you !" site="https://exodrones.com"></VendorCard>
+      <Button variant="outlined"  onClick={() => handleClick("/")} style={{marginTop: "3%"}}>
         Home
       </Button>
       </Stack>
